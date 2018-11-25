6c1694c4f6ab819373dba7503fdb2016f9ca5427
Mega Drive <Mega-Drive@users.noreply.github.com>
2018-11-25 19:17:53 +0100

    Add files via upload

--- js/episodes.js
+++ js/episodes.js
@@ -8,6 +8,7 @@
 2. Set Header
 3. Init Menu
 4. Init Favs
+5. Init SVG
 5. Init Gallery
 
 
@@ -28,6 +29,7 @@ $(document).ready(function()
 	setHeader();
 	initMenu();
 	initFavs();
+	initSvg();
 	initGallery();
 
 	$(window).on('resize', function()
@@ -118,6 +120,47 @@ $(document).ready(function()
 				});
 			});
 		}
+	}
+
+	/* 
+
+	5. Init SVG
+
+	*/
+
+	function initSvg()
+	{
+		if($('img.svg').length)
+		{
+			jQuery('img.svg').each(function()
+			{
+				var $img = jQuery(this);
+				var imgID = $img.attr('id');
+				var imgClass = $img.attr('class');
+				var imgURL = $img.attr('src');
+
+				jQuery.get(imgURL, function(data)
+				{
+					// Get the SVG tag, ignore the rest
+					var $svg = jQuery(data).find('svg');
+
+					// Add replaced image's ID to the new SVG
+					if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+					}
+					// Add replaced image's classes to the new SVG
+					if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+					}
+
+					// Remove any invalid XML tags as per http://validator.w3.org
+					$svg = $svg.removeAttr('xmlns:a');
+
+					// Replace image with new SVG
+					$img.replaceWith($svg);
+				}, 'xml');
+			});
+		}	
 	}
 
 	/* 
